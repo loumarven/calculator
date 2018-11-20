2df113d408e08a2d07d63293bbf7da12332793bf
Loumarven Payot <lou.payot@gmail.com>
2018-11-20 23:14:38 +0800

    Add initial code for display

---
+++ calc.js
@@ -0,0 +1,17 @@
+/* CONSTANTS */
+let DEL = "DEL";
+
+let input = [];
+
+function displayButtonValue(e) {
+  let display = document.querySelector(".display");
+  let buttonValue = e.target.textContent;
+
+  (buttonValue === DEL) ? input.pop() : input.push(buttonValue);
+  display.textContent = Array.from(input).join("");
+}
+
+let buttons = document.querySelectorAll(".buttons > div > div");
+buttons.forEach(button => {
+  button.addEventListener("click", displayButtonValue);
+});
