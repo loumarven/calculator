8bb5cbae3efe75dc434c25384cffdf5ad5e8f57b
Loumarven S. Payot <lou.payot@gmail.com>
2018-11-29 00:52:30 +0800

    Perform refactoring of code

--- calc.js
+++ calc.js
@@ -1,17 +1,94 @@
-/* CONSTANTS */
-let DEL = "DEL";
-
+/* GLOBALS */
 let input = [];
+let numStr = "";
+let display = document.querySelector(".display");
 
-function displayButtonValue(e) {
-  let display = document.querySelector(".display");
-  let buttonValue = e.target.textContent;
+function processOperator(e) {
+  let operator = e.target.textContent;
 
-  (buttonValue === DEL) ? input.pop() : input.push(buttonValue);
+  if (!numStr && input.length === 0) {
+    return;
+  }
+
+  if (numStr) {
+    input.push(numStr);
+  }
+
+  if (input[input.length - 1].match(/[\+\-\xD7\xF7]/)) {
+    input.pop(); // pop last operator if pressing another operator consecutively
+  }
+
+  input.push(operator);
+  numStr = "";
   display.textContent = input.join("");
 }
 
-let buttons = document.querySelectorAll(".buttons > div > div");
-buttons.forEach(button => {
-  button.addEventListener("click", displayButtonValue);
+function processNum(e) {
+  let num = e.target.textContent;
+
+  if (num === "0" && !numStr) {
+    return; // do not display leading zero for whole numbers
+  }
+
+  if (input.length === 1) {
+      input = []; // delete result from previous computation
+  }
+
+  if (num === ".") {
+    if (!numStr) {
+      num = "0." // prepend zero if num is less than 1
+    } else if (numStr.includes(".")) {
+      return; // do not allow multiple decimal points
+    }
+  }
+
+  numStr += num;
+  display.textContent = input.join("") + numStr;
+}
+
+function evaluateInput(e) {
+  if (!numStr && input.length === 0) {
+    return;
+  }
+
+  if (numStr) {
+    input.push(numStr);
+  }
+
+  if (input[input.length - 1].match(/[\+\-\xD7\xF7]/)) {
+    input.pop(); // pop last operator if no operand to its right
+  }
+
+  let expr = input.join(" ");
+  expr = expr.replace(/\xD7/g, "*"); // replace ASCII multiply symbol with *
+  expr = expr.replace(/\xF7/g, "/"); // replace ASCII divide symbol with /
+  let answer = eval(expr);
+
+  display.textContent = answer;
+  input = [answer.toString()];
+
+  numStr = "";
+}
+
+function clearAll(e) {
+  input = [];
+  numStr = "";
+  display.textContent = "0";
+}
+
+/* EVENT LISTENERS */
+let nums = document.querySelectorAll(".num");
+nums.forEach(num => {
+  num.addEventListener("click", processNum);
 });
+
+let operators = document.querySelectorAll(".operator");
+operators.forEach(operator => {
+  operator.addEventListener("click", processOperator);
+});
+
+let equals = document.querySelector("#equals");
+equals.addEventListener("click", evaluateInput);
+
+let ac = document.querySelector("#ac");
+ac.addEventListener("click", clearAll);
